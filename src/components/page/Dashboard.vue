ca85fcfcbbf31e85171bdd5b9c0ff77eefbf8326
Healer-bit <2359640299@qq.com>
2020-11-16 11:28:11 +0800

    first commit

---
+++ src/components/page/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import bus from '../common/bus';
+export default {
+    name: 'dashboard',
+    data() {
+        return {};
+    },
+    components: {
+
+    },
+    computed: {
+        role() {
+            return this.name === 'admin' ? '超级管理员' : '普通用户';
+        }
+    },
+
+    methods: {}
+};
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            redirect: '/dashboard'
+        },
+        {
+            path: '/',
+            component: () => import('../components/common/Home.vue'),
+            meta: { title: '自述文件' },
+            children: [
+                {
+                    path: '/dashboard',
+                    component: () => import(/* webpackChunkName: "dashboard" */ '../components/page/Dashboard.vue'),
+                    meta: { title: '系统首页' }
+                },
+                {
+                    path: '/table',
+                    component: () => import(/* webpackChunkName: "table" */ '../components/page/BaseTable.vue'),
+                    meta: { title: '基础表格' }
+                },{
+                    path: '/tabs',
+                    component: () => import(/* webpackChunkName: "table" */ '../components/page/Tabs.vue'),
+                    meta: { title: 'tabs' }
+                }
+            ]
+        },
+        {
+            path: '/login',
+            component: () => import(/* webpackChunkName: "login" */ '../components/page/Login.vue'),
+            meta: { title: '登录' }
+        },
+
+    ]
+});
